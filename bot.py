0bceb017d964f10b582c7f602445f11e83009fe3
Ishi-Kun <125720480+kakashi-uwu@users.noreply.github.com>
2023-03-06 00:27:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,49 +1,20 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import requests
-import random
+import telebot
 
-# Telegram Bot API Token
+# Replace with your bot token
 TOKEN = '5063237105:AAHGFmZ1RFNhil9xo0oeLDPYc_TULjAWxNM'
 
-# URL to fetch waifus from
-WAIFU_URL = 'https://api.waifu.pics/sfw/waifu'
+# Create a new bot instance using the token
+bot = telebot.TeleBot(TOKEN)
 
-# Define a function to handle the /start command
-def start(update, context):
-    update.message.reply_text('Welcome to the Waifu Catcher bot!')
+# Define a command handler
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Hello, welcome to my bot!")
 
-# Define a function to handle the /catch command
-def catch(update, context):
-    # Fetch a random waifu image
-    response = requests.get(WAIFU_URL)
-    waifu_url = response.json()['url']
-
-    # Send the waifu image to the user
-    context.bot.send_photo(chat_id=update.effective_chat.id, photo=waifu_url)
-
-# Define a function to handle messages
-def message_handler(update, context):
-    # Parse the message text
-    text = update.message.text.lower()
-
-    # Check if the message contains the word "waifu"
-    if 'waifu' in text:
-        # Fetch a random waifu image
-        response = requests.get(WAIFU_URL)
-        waifu_url = response.json()['url']
-
-        # Send the waifu image to the user
-        context.bot.send_photo(chat_id=update.effective_chat.id, photo=waifu_url)
-    else:
-        # Send a generic response
-        update.message.reply_text('Sorry, I didn\'t understand that command.')
-
-# Create the bot and add handlers
-updater = Updater(TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('catch', catch))
-dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
+# Define a message handler
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.reply_to(message, message.text)
 
 # Start the bot
-updater.start_polling()
+bot.polling()
