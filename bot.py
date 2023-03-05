1da035e782b454b6403284a04af056bd0eb9dc15
Ishi-Kun <125720480+kakashi-uwu@users.noreply.github.com>
2023-03-05 23:37:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,10 +9,17 @@ def main():
     if not TOKEN:
         print('Error: Please set the BOT_TOKEN environment variable.')
         return
+    
+def welcome(update, context):
+    message = "Welcome to our chat! Thanks for joining us."
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
 
     updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
+    welcome_handler = CommandHandler('welcome', welcome)
+    dispatcher.add_handler(welcome_handler)
 
     updater.start_polling()
 
