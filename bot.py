8553af60558b0d5449621cb2c985e2cb2eb66c12
Ishi-Kun <125720480+kakashi-uwu@users.noreply.github.com>
2023-03-06 00:23:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,49 @@
-import os
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import requests
+import random
 
+# Telegram Bot API Token
+TOKEN = '5063237105:AAHGFmZ1RFNhil9xo0oeLDPYc_TULjAWxNM'
+
+# URL to fetch waifus from
+WAIFU_URL = 'https://api.waifu.pics/sfw/waifu'
+
+# Define a function to handle the /start command
 def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Hello! I'm a bot.")
+    update.message.reply_text('Welcome to the Waifu Catcher bot!')
 
-def main():
-    TOKEN = os.environ.get('BOT_TOKEN')
-    if not TOKEN:
-        print('Error: Please set the BOT_TOKEN environment variable.')
-        return
-    
-def welcome(update, context):
-    message = "Welcome to our chat! Thanks for joining us."
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+# Define a function to handle the /catch command
+def catch(update, context):
+    # Fetch a random waifu image
+    response = requests.get(WAIFU_URL)
+    waifu_url = response.json()['url']
 
+    # Send the waifu image to the user
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=waifu_url)
 
-    updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
-    welcome_handler = CommandHandler('welcome', welcome)
-    dispatcher.add_handler(welcome_handler)
+# Define a function to handle messages
+def message_handler(update, context):
+    # Parse the message text
+    text = update.message.text.lower()
 
-    updater.start_polling()
+    # Check if the message contains the word "waifu"
+    if 'waifu' in text:
+        # Fetch a random waifu image
+        response = requests.get(WAIFU_URL)
+        waifu_url = response.json()['url']
 
-if __name__ == '__main__':
-    main()
+        # Send the waifu image to the user
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=waifu_url)
+    else:
+        # Send a generic response
+        update.message.reply_text('Sorry, I didn\'t understand that command.')
+
+# Create the bot and add handlers
+updater = Updater(TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('catch', catch))
+dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
+
+# Start the bot
+updater.start_polling()
