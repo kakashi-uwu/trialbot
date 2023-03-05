92fb3f7519e99479c487cafa8d8e7da88dc9e581
Ishi-Kun <125720480+kakashi-uwu@users.noreply.github.com>
2023-03-06 00:38:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import os
 from telegram.ext import Updater, CommandHandler
 
+START_PG="https://graph.org//file/7b263b04a2fe3559bd769.jpg"
 def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text="Hello! I'm a bot.")
+    context.bot.send_photo(chat_id=update.message.chat_id, photo="START_PG")
 
 def main():
     TOKEN = os.environ.get('BOT_TOKEN')
