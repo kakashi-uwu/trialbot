ba277f4e3990d2a77d57aab399c398a622b82a94
Ishi-Kun <125720480+kakashi-uwu@users.noreply.github.com>
2023-03-06 11:28:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,20 @@
 import telegram
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-
-start_image="https://graph.org//file/7b263b04a2fe3559bd769.jpg"
-# Replace YOUR_TOKEN_HERE with your actual Telegram bot token
-bot = telegram.Bot(token='5792603393:AAEIi1EyAToI0bMNp5WkVXb7kdd7HLKCjNw')
+TOKEN = '5792603393:AAEIi1EyAToI0bMNp5WkVXb7kdd7HLKCjNw'
 
 def start(update, context):
-    chat_id = update.message.chat_id
-    context.bot.send_photo(chat_id=chat_id, photo=open('start_image'))
+    context.bot.send_message(chat_id=update.message.chat_id, text="Hello, I'm a bot!")
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
 def main():
-    updater = Updater(token='5792603393:AAEIi1EyAToI0bMNp5WkVXb7kdd7HLKCjNw', use_context=True)
-    dispatcher = updater.dispatcher
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
 
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
     updater.start_polling()
     updater.idle()
