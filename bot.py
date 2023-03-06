42a44ac70bc04bdadaeebcd435ca7ff0bd983886
Ishi-Kun <125720480+kakashi-uwu@users.noreply.github.com>
2023-03-06 11:01:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,27 @@
 import os
 from telegram.ext import Updater, CommandHandler
 
+START_IMG="https://graph.org//file/7b263b04a2fe3559bd769.jpg"
+
 def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text="Hello! I'm a bot.")
+    
+def fstart(update, context):    
+       first_name = update.effective_user.first_name
+       update.effective_message.reply_animation(
+            START_IMG, caption= "hey there im a trial bot".format(
+             first_name,uptime
+            ),
+            parse_mode=ParseMode.MARKDOWN,
+        reply_markup=InlineKeyboardMarkup(
+                [
+                  [
+                  InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://telegram.dog/{SUPPORT_CHAT}"),
+                  InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇs", url=f"t.me/{UPDATES_CHANNEL}"),
+                  ]
+                ]
+            ),
+        )
 
 def main():
     TOKEN = os.environ.get('BOT_TOKEN')
@@ -13,6 +32,7 @@ def main():
     updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
+    fstart_handler = DisableAbleCommandHandler("fstart", fstart)
 
     updater.start_polling()
 
