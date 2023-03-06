4100c20616e81a1342e248d5cf2e739d8be07ba7
Ishi-Kun <125720480+kakashi-uwu@users.noreply.github.com>
2023-03-06 10:44:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,24 @@
 import os
 from telegram.ext import Updater, CommandHandler
 
-START_PG="https://graph.org//file/7b263b04a2fe3559bd769.jpg"
 def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text="Hello! I'm a bot.")
-    context.bot.send_photo(chat_id=update.message.chat_id, photo="START_PG")
 
 def main():
     TOKEN = os.environ.get('BOT_TOKEN')
     if not TOKEN:
         print('Error: Please set the BOT_TOKEN environment variable.')
         return
+    
+ def handle_message(update, context):
+    text = update.message.text
+    if text == '/gstart':
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! Type /game to play my game.")
+    elif text == '/game':
+        # start the game here
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Let's play my game!")
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm sorry, I don't understand that command.")
 
     updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
