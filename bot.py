0ea0f058ffaa71b913304855f69dacf54ec05ec0
Ishi-Kun <125720480+kakashi-uwu@users.noreply.github.com>
2023-03-06 11:14:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,11 @@
 import os
 from telegram.ext import Updater, CommandHandler
 
-START_IMG="https://graph.org//file/7b263b04a2fe3559bd769.jpg"
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Hello! I'm a bot.")
+    context.bot.send_message(chat_id=update.message.chat_id, text="Hello! I'm a bot("https://graph.org//file/7b263b04a2fe3559bd769.jpg").")
     
-def fstart(update, context):    
-       update.effective_message.reply_animation(
-            START_IMG, caption= "hey there im a trial bot".format(
-             first_name,uptime
-            ),
-            parse_mode=ParseMode.MARKDOWN,
-        reply_markup=InlineKeyboardMarkup(
-                [
-                  [
-                  InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://telegram.dog/{SUPPORT_CHAT}"),
-                  InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇs", url=f"t.me/{UPDATES_CHANNEL}"),
-                  ]
-                ]
-            ),
-        )
+
 
 def main():
     TOKEN = os.environ.get('BOT_TOKEN')
@@ -31,8 +16,6 @@ def main():
     updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
-    fstart_handler = CommandHandler("fstart", start)
-
     updater.start_polling()
 
 if __name__ == '__main__':
