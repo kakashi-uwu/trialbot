aa688b36bae0f946d98d1a4e8191ddfe329bcbed
Ishi-Kun <125720480+kakashi-uwu@users.noreply.github.com>
2023-03-11 14:18:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,62 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 TOKEN = '5792603393:AAEIi1EyAToI0bMNp5WkVXb7kdd7HLKCjNw'
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Hello, I'm a bot!")
+def start(update: Update, context: CallbackContext):
+    args = context.args
+    uptime = get_readable_time((time.time() - StartTime))
+    if update.effective_chat.type == "private":
+        if len(args) >= 1:
+            if args[0].lower() == "help":
+                send_help(update.effective_chat.id, HELP_STRINGS)
+            elif args[0].lower().startswith("ghelp_"):
+                mod = args[0].lower().split("_", 1)[1]
+                if not HELPABLE.get(mod, False):
+                    return
+                send_help(
+                    update.effective_chat.id,
+                    HELPABLE[mod].__help__,
+                    InlineKeyboardMarkup(
+                        [[InlineKeyboardButton(text="⬅Back", callback_data="help_back")]]
+                    ),
+                )
+
+            elif args[0].lower().startswith("stngs_"):
+                match = re.match("stngs_(.*)", args[0].lower())
+                chat = dispatcher.bot.getChat(match.group(1))
+
+                if is_user_admin(chat, update.effective_user.id):
+                    send_settings(match.group(1), update.effective_user.id, False)
+                else:
+                    send_settings(match.group(1), update.effective_user.id, True)
+
+            elif args[0][1:].isdigit() and "rules" in IMPORTED:
+                IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
+
+        else:
+            first_name = update.effective_user.first_name
+            update.effective_message.reply_photo(
+               random.choice(HINATA_IMG),PM_START_TEXT.format(first_name),
+                reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=ParseMode.MARKDOWN,
+                timeout=60,
+            )
+    else:
+        first_name = update.effective_user.first_name
+        update.effective_message.reply_animation(
+            GROUPSTART_IMG, caption= "*hello!\n ┗► {} ◄┛,*\n*I'm Hinata hyuga*\n*Haven't slept since* : {} ".format(
+             first_name,uptime
+            ),
+            parse_mode=ParseMode.MARKDOWN,
+        reply_markup=InlineKeyboardMarkup(
+                [
+                  [
+                  InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://telegram.dog/{SUPPORT_CHAT}"),
+                  InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇs", url=f"t.me/{UPDATES_CHANNEL}"),
+                  ]
+                ]
+            ),
+        )
+
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
