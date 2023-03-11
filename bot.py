4c53bb71bc073505c78935ea88c26f221c191ab8
Ishi-Kun <125720480+kakashi-uwu@users.noreply.github.com>
2023-03-11 14:56:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,13 @@ import os
 from telegram.ext import Updater, CommandHandler
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Hello! I'm a bot.")
+          first_name = update.effective_user.first_name
+          update.effective_message.reply_photo(
+               random.choice(START_IMG),PM_START_TEXT.format(first_name),
+                reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=ParseMode.MARKDOWN,
+                timeout=60,
+            )
     
 def main():
     TOKEN = os.environ.get('BOT_TOKEN')
